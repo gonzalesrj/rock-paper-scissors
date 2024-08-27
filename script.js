ae1b576c3f87808ecc197e878c6dd8b50e41623c
Russel John Gonzales <russeljohn.gonzales@yahoo.com>
2024-08-27 02:30:30 -0400

    Add a function that takes a value from html button

--- script.js
+++ script.js
@@ -3,57 +3,66 @@ let computerScore;
 let rounds;
 let getAnswer;
 
-const getComputerChoice = () => {
-  let randomNumber =  Math.floor(Math.random() * 3);
-
-  return (randomNumber === 0) ? "rock"
-    : (randomNumber === 1) ? "paper"
-    : "scissor";
+function getHumanChoice() {
+  console.log(this.value);
 }
+let answerButton = document.querySelectorAll(".answer");
 
-const getHumanChoice = () => {
-  let answer = getAnswer.toLowerCase();
-  return answer;
-}
+answerButton.forEach(answer => {
+  answer.addEventListener("click", getHumanChoice, true);
+});
 
-const playRound = (humanChoice, computerChoice) => {
 
-  if(humanChoice === computerChoice) {
-    return `You chose ${humanChoice} and computer chose ${computerChoice}. It's a draw!`;
-  } else if((humanChoice === "rock" && computerChoice === "scissor") 
-      || (humanChoice === "paper" && computerChoice ==="rock")
-      || (humanChoice === "scissor" && computerChoice === "paper")) {
-      humanScore++;
-      return `You win! ${humanChoice} beats ${computerChoice}`;  
-  } else {
-    computerScore++;
-    return `You lose! ${computerChoice} beats ${humanChoice}`;
-  }
-}
 
-const playGame = () => {
-  rounds = 0;
-  humanScore = 0;
-  computerScore = 0;
+// const getComputerChoice = () => {
+//   let randomNumber =  Math.floor(Math.random() * 3);
 
-  while(rounds !== 5) {
-    getAnswer = prompt("Enter your answer");
+//   return (randomNumber === 0) ? "rock"
+//     : (randomNumber === 1) ? "paper"
+//     : "scissor";
+// }
 
-    const computerSelection = getComputerChoice();
-    const humanSelection = getHumanChoice();
 
-    console.log(playRound(humanSelection, computerSelection));
-    console.log(`Your score: ${humanScore} Computer Score: ${computerScore}`);
 
-    rounds++;
-  } 
 
-  if (humanScore > computerScore) {
-    console.log("You win this game");
-  } else {
-    console.log("Sorry! You lose. Try again.");
-  }
-}
+// const playRound = (humanChoice, computerChoice) => {
 
-playGame();
+//   if(humanChoice === computerChoice) {
+//     return `You chose ${humanChoice} and computer chose ${computerChoice}. It's a draw!`;
+//   } else if((humanChoice === "rock" && computerChoice === "scissor") 
+//       || (humanChoice === "paper" && computerChoice ==="rock")
+//       || (humanChoice === "scissor" && computerChoice === "paper")) {
+//       humanScore++;
+//       return `You win! ${humanChoice} beats ${computerChoice}`;  
+//   } else {
+//     computerScore++;
+//     return `You lose! ${computerChoice} beats ${humanChoice}`;
+//   }
+// }
+
+// const playGame = () => {
+//   rounds = 0;
+//   humanScore = 0;
+//   computerScore = 0;
+
+//   while(rounds !== 5) {
+//     getAnswer = prompt("Enter your answer");
+
+//     const computerSelection = getComputerChoice();
+//     const humanSelection = getHumanChoice();
+
+//     console.log(playRound(humanSelection, computerSelection));
+//     console.log(`Your score: ${humanScore} Computer Score: ${computerScore}`);
+
+//     rounds++;
+//   } 
+
+//   if (humanScore > computerScore) {
+//     console.log("You win this game");
+//   } else {
+//     console.log("Sorry! You lose. Try again.");
+//   }
+// }
+
+// playGame();
 
